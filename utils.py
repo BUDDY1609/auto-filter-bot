c1397db6ded6942402c62211e21720472febb1ef
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2023-01-05 00:04:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -464,21 +464,23 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://dulink.in/api'
-    params = {'api': SHORTENER_API,
-              'url': link,
+        
+    url = f'https://api.shareus.in/shortLink'
+    params = {'token': URL_SHORTNER_WEBSITE_API,
+              'link': link,
+              'format': 'json'
               }
 
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
+                data = await response.json(content_type='text/html')
                 if data["status"] == "success":
-                    return data['shortenedUrl']
+                    return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://dulink.in/api?api={SHORTENER_API}&link={link}'
+                    return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://dulink.in/api?api={SHORTENER_API}&link={link}'
+        return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
